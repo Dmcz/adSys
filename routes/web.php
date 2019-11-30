b9575915f600931b9d6558a7bbb01a5d3af2060e
Dmcz <751662789@163.com>
2019-11-30 16:30:51 +0800

    '路由调整'

--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,15 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes([
     'register' => false
 ]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/questionnaire/{no}', 'HomeController@questionnaire')->name('questionnaire');
 Route::post('/questionnaire/{no}', 'HomeController@questionnaireSave')->name('questionnaire.save');
 
